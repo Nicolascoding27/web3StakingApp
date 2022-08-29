7ef17e4f369797c1e2026bd0b6749ac06c4dd52d
Nicolas Felipe Diaz <68035598+Nicolascoding27@users.noreply.github.com>
2022-08-29 16:37:04 -0500

    Change added

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
@@ -7,14 +7,39 @@ import "./ExampleExternalContract.sol";
 contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
+  address public owner;
+  address public contrato;
+  uint public constant threshold =1 ether; 
 
   constructor(address exampleExternalContractAddress) {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+      owner = msg.sender;
+      // threshold=1.0;
   }
+  
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
-
+  mapping (address => uint256) public balances; //Always add public 
+function stake(uint value) payable public  {
+  uint balance = balances[msg.sender];
+  // contrato= 0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e;
+  // balances[contrato]+= value.BigNumber; 
+  // balance-=value.BigNumber;
+   console.log("BALANCE");
+  console.log(balance);
+   console.log("NUMBER");
+  console.log(value);
+}
+// function stake (address destino, uint value) payable public {
+//         payable(destino).transfer(value);
+//     }
+// function trackBalcances (address user) public returns (uint){
+//   uint balance =  balances[user];
+//   console.log("BALANCE");
+//   console.log(balance);
+//   return balance;
+// } 
 
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
@@ -30,6 +55,6 @@ contract Staker {
 
 
   // Add the `receive()` special function that receives eth and calls stake()
-
+  
 
 }
