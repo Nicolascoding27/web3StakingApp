54b6fa26f175705e3e586e64b8ef81e4538007f0
Nicolas Felipe Diaz <68035598+Nicolascoding27@users.noreply.github.com>
2022-08-29 17:01:55 -0500

    Staking contract working

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -21,15 +21,19 @@ contract Staker {
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
   mapping (address => uint256) public balances; //Always add public 
-function stake(uint value) payable public  {
-  uint balance = balances[msg.sender];
+  event Stake (address from, uint ammount); //This will notify when the stake changes 
+function stake() public payable  {
+
+  // uint balance = balances[msg.sender] += msg.value; //This is wrong what is coming is a big number and I am asociating it to a uint 
+  balances[msg.sender] += msg.value;
+  //Emit event, stake 
+  emit Stake(msg.sender, msg.value);
   // contrato= 0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e;
   // balances[contrato]+= value.BigNumber; 
   // balance-=value.BigNumber;
-   console.log("BALANCE");
-  console.log(balance);
+   console.log("COMPLETED");
+  console.log(balances[msg.sender]);
    console.log("NUMBER");
-  console.log(value);
 }
 // function stake (address destino, uint value) payable public {
 //         payable(destino).transfer(value);
